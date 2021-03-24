8be37ba2d5323b337143fed0e95cb8647de3b2f3
Ömer ÜCEL <omerucel@gmail.com>
2021-03-24 03:01:38 +0300

    fix certificate issues #MOD-38

--- backend/src/consumer/deployer/terraform_templates/main.tf
+++ backend/src/consumer/deployer/terraform_templates/main.tf
@@ -3,3 +3,10 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
