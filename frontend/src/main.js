a8fc6990a5d8a3aff395985dd9bce4a52ab4c45a
Ömer ÜCEL <omerucel@gmail.com>
2021-04-10 02:02:01 +0300

    check token before app mount #MOD-64

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+Vue.config.productionTip = false
 import Notifications from 'vue-notification'
 Vue.use(Notifications);
 
@@ -15,17 +16,6 @@ import './assets/app.scss';
 import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
-firebase.initializeApp({
-    apiKey: "AIzaSyAV5XeHaikSuThrfDnOjBeUXarz5lNzJbI",
-    authDomain: "moderncloud---test.firebaseapp.com",
-    projectId: "moderncloud---test",
-    storageBucket: "moderncloud---test.appspot.com",
-    messagingSenderId: "247003552904",
-    appId: "1:247003552904:web:94ed3b9afed3cff9b8133d"
-});
-
-
-document.getElementById('loading').remove();
 
 axios.interceptors.request.use(
     reqConfig => {
@@ -35,24 +25,46 @@ axios.interceptors.request.use(
     err => Promise.reject(err)
 );
 
-axios.interceptors.response.use(
-    function (response) {
-        return response;
-    },
-    function (error) {
-        if (error.response.status === 401) {
+checkToken();
+function checkToken() {
+    axios.get('/api/auth/my-info')
+        .catch(e => {
+            console.log(e);
             store.commit('clearSelectedProject');
             store.commit('logout');
-            router.push({path: '/login'});
+        })
+        .finally(run);
+}
+
+async function run() {
+    document.getElementById('loading').remove();
+
+    firebase.initializeApp({
+        apiKey: "AIzaSyAV5XeHaikSuThrfDnOjBeUXarz5lNzJbI",
+        authDomain: "moderncloud---test.firebaseapp.com",
+        projectId: "moderncloud---test",
+        storageBucket: "moderncloud---test.appspot.com",
+        messagingSenderId: "247003552904",
+        appId: "1:247003552904:web:94ed3b9afed3cff9b8133d"
+    });
+
+    axios.interceptors.response.use(
+        function (response) {
+            return response;
+        },
+        function (error) {
+            if (error.response.status === 401) {
+                store.commit('clearSelectedProject');
+                store.commit('logout');
+                router.push({path: '/login'});
+            }
+            return Promise.reject(error);
         }
-        return Promise.reject(error);
-    }
-);
+    );
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    new Vue({
+        router,
+        store,
+        render: h => h(App)
+    }).$mount('#app')
+}
