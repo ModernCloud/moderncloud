31e4a21b59d1584592e14f5cdedeba05b0bc43dd
Ömer ÜCEL <omerucel@gmail.com>
2021-05-09 22:25:46 +0300

    fix packages issues

--- backend/src/api/package/update_packages_folder.js
+++ backend/src/api/package/update_packages_folder.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
+const path = require('path');
 const { Package } = require('../../common/db');
 const render = require('../../common/template/render');
+const shelljs = require('shelljs');
 
 module.exports = async (projectId) => {
     let projectPackages = process.env.STORAGE + '/projects/' + projectId + '/packages';
-    let packages = await Package.query().where('project_id', projectId).groupBy(['name']);
+    let packages = await Package.query()
+        .select('name')
+        .select(Package.raw('MAX(version) AS version'))
+        .where('project_id', projectId).groupBy(['name']);
     fs.mkdirSync(projectPackages, {recursive: true});
     render(
         path.join(__dirname, 'package.json.twig'),
         path.join(projectPackages, 'package.json'),
         {packages: packages}
     )
+    shelljs.exec(`npm install --no-fund --no-audit --prod --no-optional --ignore-scripts --no-bin-links --prefix "${projectPackages}"`, {async:true, silent: true});
 };
