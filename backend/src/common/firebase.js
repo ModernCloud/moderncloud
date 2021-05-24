7f4bb2beb6ea78d596a380ba9e6ae53fdc66c5ba
Ömer ÜCEL <omerucel@gmail.com>
2021-05-25 01:30:36 +0300

    remove unnecessary firebase files

--- backend/src/common/firebase.js
+++
@@ -1,5 +0,0 @@
-const firebaseAdmin = require('firebase-admin');
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(require(process.env.FIREBASE_CONFIG))
-});
-module.exports = firebaseAdmin;
